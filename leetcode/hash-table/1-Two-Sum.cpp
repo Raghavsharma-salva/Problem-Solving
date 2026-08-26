d6bf642fa9afe8de07fc85d0234c31f26d9bad6c
Raghav Sharma <raghavsharma282007@gmail.com>
2026-08-26 15:12:05 +0530

    Add Two-Sum (hash-table)

---
+++ leetcode/hash-table/1-Two-Sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size() ; i++){
+            for(int j = 1 + i;j < nums.size() ;j++){
+                if(target == nums[i] + nums[j]){
+                    return{i,j};
+                }
+            }
+        }
+        return {};
+    }
+};
