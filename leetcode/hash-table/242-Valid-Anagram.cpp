31abb03ffdc9a2a9800f4f7501f73c3a22ec98d7
Raghav Sharma <raghavsharma282007@gmail.com>
2026-09-04 11:31:00 +0530

    Add Valid-Anagram (hash-table)

---
+++ leetcode/hash-table/242-Valid-Anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       std::sort(s.begin(),s.end());
+       std::sort(t.begin(),t.end());
+        int n = s.length();
+        bool flag = true;
+        if(n != t.length()){
+            return false;
+        }
+       for(int i = 0; i < n ; i++){
+            if(s[i] != t[i]){
+                flag = false;
+            }
+       } 
+
+       return flag;
+    }
+};
